7375c6c023fe7c7994a0a4c157eeba69e6033f08
sppedro11 <pedrosimoes106@usp.br>
2023-11-26 18:55:03 -0300

    Update codigo2.py

--- codigo2.py
+++ codigo2.py
@@ -13,16 +13,16 @@ def obter_senha_encriptada():
     with open("senha_encriptada.txt", "r") as arquivo:
         return arquivo.read().strip()
 
-def verificar_senha(senha_digitada, senha_encriptada):
-    senha_encriptada_digitada = hashlib.sha256(senha_digitada.encode()).hexdigest()
-    return senha_encriptada_digitada == senha_encriptada
 
 def main():
     senha = obter_senha()
     salvar_senha_encriptada(senha)
     senha_encriptada = obter_senha_encriptada()
-    senha_digitada = 'pedro123' 
-    assert verificar_senha(senha_digitada,senha_encriptada)
+    senha_digitada = 'pedro123'
+
+def verificar_senha(senha_digitada, senha_encriptada):
+    senha_encriptada_digitada = hashlib.sha256(senha_digitada.encode()).hexdigest()
+    assert senha_encriptada_digitada == senha_encriptada
 
 if __name__ == "__main__":
     main()
